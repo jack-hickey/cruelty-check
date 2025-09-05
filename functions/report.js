de5bbbc113dea8ad92f09a3857b1d557e6340f43
Jack Hickey <jack.hickey@chipside.com>
2025-09-05 12:41:52 +0100

    feat: added data to git issues

--- functions/report.js
+++ functions/report.js
@@ -3,13 +3,39 @@ export async function onRequest(context) {
     return new Response("Method Not Allowed", { status: 405 });
   }
 
-	const data = await context.request.json();
+	let data = await context.request.json(),
+		browser = "Unknown",
+		userAgent = navigator.userAgent,
+		os = "Unknown",
+		platform = navigator.platform.toLowerCase();
+
+	if (userAgent.includes("Chrome") && !userAgent.includes("Edge") && !userAgent.includes("OPR")) { browser = "Chrome"; }
+	else if (userAgent.includes("Firefox")) { browser = "Firefox"; }
+	else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) { browser = "Safari"; }
+	else if (userAgent.includes("Edge")) { browser = "Edge"; }
+	else if (userAgent.includes("OPR") || userAgent.includes("Opera")) { browser = "Opera"; }
+
+	if (platform.includes("win")) { os = "Windows"; }
+	else if (platform.includes("mac")) { os = "macOS"; }
+	else if (platform.includes("linux")) { os = "Linux"; }
+	else if (/iphone|ipad|ipod/.test(userAgent.toLowerCase())) { os = "iOS"; }
+	else if (/android/.test(userAgent.toLowerCase())) { os = "Android"; }
 
   if (!data.title || !data.description || !data.type) {
     return new Response("Bad Request", { status: 400 });
   }
 
-  const issueBody = `**Type:** ${data.type}\n**Description:**\n${data.description}`;
+	const issueBody = 
+	`
+		**Browser**
+		${browser}
+
+		**Operating System**
+		${os}
+
+		**Details**
+		${data.description}
+	`;
 
   const response = await fetch(`https://api.github.com/repos/${context.env.GITHUB_REPO}/issues`, {
     method: 'POST',
