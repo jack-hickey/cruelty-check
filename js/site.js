b5b4375c057581cee231563192184d584c584e90
Jack Hickey <jack.hickey@chipside.com>
2025-09-23 10:32:57 +0100

    revert: removed service worker

--- js/site.js
+++ js/site.js
@@ -1,7 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.min.js");
-}
-
 document.querySelectorAll(".btn--feedback").forEach(button => Object.assign(button, {
 	textContent: Localizer.FEEDBACK_BUTTON_LABEL,
 	onclick: () => {
